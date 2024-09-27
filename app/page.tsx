bae67e0103c6d39d5bfb1d32e4d052a1eb4c4db0
Yash Burshe <bursheyash2@gmail.com>
2024-09-26 21:28:07 -0400

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      Ez Meal Prep
+    <div className="w-96 mt-20 mx-auto text-center">
+      <h1 className="font-bold text-2xl border-b pb-4">EZ Meal Prep</h1>
+      <p className="text-lg mt-2">Plan your meals and grocery trips</p>
     </div>
   );
 }
