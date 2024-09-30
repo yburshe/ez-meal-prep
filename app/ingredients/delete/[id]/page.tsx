0a08127b9cc4ab285541295f9a53ae822e6a32a3
Yash Burshe <bursheyash2@gmail.com>
2024-09-29 21:12:17 -0400

    Add delete ingredient page

--- app/ingredients/delete/[id]/page.tsx
+++ app/ingredients/delete/[id]/page.tsx
@@ -1,9 +1,28 @@
-export default function DeleteIngredient({
+import { deleteIngredient, getIngredientDetails } from "@/app/actions";
+import Link from "next/link";
+
+export default async function DeleteIngredient({
   params,
 }: {
   params: {
     id: number;
   };
 }) {
-  return <div>DeleteIngredient {params.id}</div>;
+  const ingredientDetails = await getIngredientDetails(params.id);
+  const ingredientName = ingredientDetails[0].name;
+
+  return (
+    <div className="flex shadow-sm w-fit mx-auto justify-center flex-col border rounded-md text-center">
+      <h1 className="px-4 py-2 text-center border-b text-xl font-semibold">
+      ⚠️ You are about to delete {ingredientName}
+      </h1>
+      <div className="p-4 flex justify-between">
+        <Link className="shadow-sm border border-neutral-300 bg-neutral-100 hover:bg-neutral-200 px-4 py-2 rounded-md" href="/ingredients">Cancel</Link>
+        <form action={deleteIngredient}>
+          <input type="hidden" name="id" value={params.id} />
+          <button className="shadow-sm border border-red-300 bg-red-100 hover:bg-red-200 px-4 py-2 rounded-md">Delete</button>
+        </form>
+      </div>
+    </div>
+  );
 }
