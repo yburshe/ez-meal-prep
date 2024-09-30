d752a73eac0329499d97822331b9235059a97ded
Yash Burshe <bursheyash2@gmail.com>
2024-09-29 21:12:06 -0400

    Add ingredient information

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -1,15 +1,15 @@
-import { db } from "@/db/db";
-import { ingredient } from "@/db/schema";
 import Link from "next/link";
 import { LuPencil, LuPlus, LuTrash } from "react-icons/lu";
+import { getIngredients } from "../actions";
+import Card from "../components/Card";
 
 export default async function Ingredients() {
-  const ingredients = await db.select().from(ingredient);
+  const ingredients = await getIngredients();
 
   return (
     <div>
       <Link
-        className="shadow-md border absolute bottom-10 right-10 p-4 rounded-full"
+        className="bg-black text-white hover:bg-neu hover:shadow-lg fixed shadow-md border bottom-10 right-10 p-4 rounded-full"
         href="/ingredients/new"
       >
         <LuPlus className="text-4xl" />
@@ -17,9 +17,9 @@ export default async function Ingredients() {
       <div className="mb-4 border-b p-2">
         <h1 className="text-2xl font-bold">Ingredients</h1>
       </div>
-      <ul className="grid grid-cols-2 gap-4">
-        {ingredients.slice(1).map((ingredient) => (
-          <div className="border rounded-sm shadow-sm" key={ingredient.id}>
+      <ul className="flex flex-wrap gap-4">
+        {ingredients.map((ingredient) => (
+          <Card key={ingredient.id}>
             <div className="p-2 border-b flex justify-between">
               <Link
                 className="text-lg font-semibold"
@@ -37,32 +37,48 @@ export default async function Ingredients() {
               </div>
             </div>
             <div className="p-2">
-              <table>
+              <p className="text-xl font-semibold mb-2">
+                ${ingredient.price}{" "}
+                <span className="text-sm font-medium">
+                  at {ingredient.storeName}
+                </span>
+              </p>
+              <p className="text-lg">
+                {ingredient.servingsPerPack}{" "}
+                <span className="text-sm">servings per pack</span>
+              </p>
+              <p className="text-lg">
+                {ingredient.servingSize}{" "}
+                <span className="text-sm">serving size</span>
+              </p>
+            </div>
+            <div className="p-2">
+              <table className="border-collapse border">
                 <thead>
                   <tr>
-                    <th>Cal</th>
-                    <th>Fat</th>
-                    <th>Carbs</th>
-                    <th>Protein</th>
-                    <th>Fiber</th>
-                    <th>Chol</th>
-                    <th>Sodium</th>
+                    <th className="border p-2">Calories</th>
+                    <th className="border p-2">Total Fat</th>
+                    <th className="border p-2">Total Carbs</th>
+                    <th className="border p-2">Protein</th>
+                    <th className="border p-2">Fiber</th>
+                    <th className="border p-2">Chol</th>
+                    <th className="border p-2">Sodium</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td>{ingredient.calories}</td>
-                    <td>{ingredient.total_fat}</td>
-                    <td>{ingredient.total_carbs}</td>
-                    <td>{ingredient.protein}</td>
-                    <td>{ingredient.fiber}</td>
-                    <td>{ingredient.cholesterol}</td>
-                    <td>{ingredient.sodium}</td>
+                    <td className="border p-2">{ingredient.calories}</td>
+                    <td className="border p-2">{ingredient.totalFat}</td>
+                    <td className="border p-2">{ingredient.totalCarbs}</td>
+                    <td className="border p-2">{ingredient.protein}</td>
+                    <td className="border p-2">{ingredient.fiber}</td>
+                    <td className="border p-2">{ingredient.cholesterol}</td>
+                    <td className="border p-2">{ingredient.sodium}</td>
                   </tr>
                 </tbody>
               </table>
             </div>
-          </div>
+          </Card>
         ))}
       </ul>
     </div>
