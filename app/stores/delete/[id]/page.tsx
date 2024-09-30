63fc92877b6255ef4699023494ff36df763e8088
Yash Burshe <bursheyash2@gmail.com>
2024-09-29 21:14:27 -0400

    Redesign delete page

--- app/stores/delete/[id]/page.tsx
+++ app/stores/delete/[id]/page.tsx
@@ -12,15 +12,15 @@ export default async function DeleteStore({
   const storeName = storeDetails[0].name;
 
   return (
-    <div className="flex shadow-sm w-fit mx-auto justify-center flex-col border rounded-sm">
-      <h1 className="p-2 border-b text-xl font-semibold">
-        You are about to delete {storeName}
+    <div className="flex shadow-sm w-fit mx-auto justify-center flex-col border rounded-md text-center">
+      <h1 className="px-4 py-2 text-center border-b text-xl font-semibold">
+      ⚠️ You are about to delete {storeName}
       </h1>
-      <div className="p-2 flex justify-between">
-        <Link href="/stores">No</Link>
+      <div className="p-4 flex justify-between">
+        <Link className="shadow-sm border border-neutral-300 bg-neutral-100 hover:bg-neutral-200 px-4 py-2 rounded-md" href="/stores">Cancel</Link>
         <form action={deleteStore}>
           <input type="hidden" name="id" value={params.id} />
-          <button>Yes</button>
+          <button className="shadow-sm border border-red-300 bg-red-100 hover:bg-red-200 px-4 py-2 rounded-md">Delete</button>
         </form>
       </div>
     </div>
