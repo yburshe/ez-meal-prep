4537f4bd4fc7e46632a31d935eaca60f6ed2a00a
Yash Burshe <bursheyash2@gmail.com>
2024-09-29 21:10:42 -0400

    Add accent color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        neu: 'rgba(204,54,58)'
       },
     },
   },
